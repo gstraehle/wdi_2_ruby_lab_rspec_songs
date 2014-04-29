15349c7296edd2338ddfcbecf0c7f8fc0211e305
George Straehle <gstraehle@gmail.com>
2014-04-29 17:14:28 -0400

    YESSSS!!!!!!!

--- lib/album.rb
+++ lib/album.rb
@@ -1,11 +1,32 @@
 require_relative '../lib/track'
+require 'pry'
 
 class Album
 
-  attr_reader :album_title, :song_list
+  attr_reader :album_title, :tracks, :song_list
 
-  def initialize(album_title:, song_list:)
+  def initialize(album_title:, tracks:)
     @album_title = album_title
-    @song_list = song_list
+    @tracks = tracks
+  end
+
+  def song_list
+    tracks.map { |track| track.title }
+  end
+
+  def runtime
+    tracks.map { |track| track.duration }.reduce(:+)
+  end
+
+  def total_price
+    tracks.map { |i| i.price }.reduce(:+)
+  end
+
+  def all_artists
+    tracks.map{|track| track.artists }.reduce(:+).uniq.sort
   end
 end
+
+#binding.pry
+
+
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -6,10 +6,32 @@ require_relative '../lib/track'
 describe Album do
   it 'has album title and song list' do
     track1 = Track.new(title: 'track_title1', artists: ['Beatles', 'Led Zeppelin', '2Pac', 'Metallica'], duration: 240, price: 1.00)
-    track2 = Track.new(title: 'track_title2', artists: ['Beetles', 4, '2Pac', 'Metallica'], duration: 10, price: 2.00)
-    album1 = Album.new(album_title: 'God', song_list: [track1.title, track2.title])
+    track2 = Track.new(title: 'track_title2', artists: ['Beetles', '4', '2Pac', 'Metallica'], duration: 10, price: 2.00)
+    album1 = Album.new(album_title: 'God', tracks: [track1, track2])
 
     expect(album1.album_title).to eq 'God'
     expect(album1.song_list).to match_array ['track_title1', 'track_title2']
+  end
+  it 'finds the total runtime' do
+    track1 = Track.new(title: 'track_title1', artists: ['Beatles', 'Led Zeppelin', '2Pac', 'Metallica'], duration: 240, price: 1.00)
+    track2 = Track.new(title: 'track_title2', artists: ['Beetles', '4', '2Pac', 'Metallica'], duration: 10, price: 2.00)
+    album1 = Album.new(album_title: 'God', tracks: [track1, track2])
+
+    expect(album1.runtime).to eq 250
+  end
+  it 'has a total price that totals all song prices' do
+    track1 = Track.new(title: 'track_title1', artists: ['Beatles', 'Led Zeppelin', '2Pac', 'Metallica'], duration: 240, price: 1.00)
+    track2 = Track.new(title: 'track_title2', artists: ['Beetles', '4', '2Pac', 'Metallica'], duration: 10, price: 2.00)
+    album1 = Album.new(album_title: 'God', tracks: [track1, track2])
+
+    expect(album1.total_price).to eq 3.00
+  end
+  it 'has a list of unique artists in alphabetical order' do
+    track1 = Track.new(title: 'track_title1', artists: ['Beatles', 'Led Zeppelin', '2Pac', 'Metallica'], duration: 240, price: 1.00)
+    track2 = Track.new(title: 'track_title2', artists: ['Beetles', '4', '2Pac', 'Metallica'], duration: 10, price: 2.00)
+    album1 = Album.new(album_title: 'God', tracks: [track1, track2])
+
+    expect(album1.all_artists).to eq ['2Pac', '4', 'Beatles', 'Beetles', 'Led Zeppelin', 'Metallica']
+
   end
 end
